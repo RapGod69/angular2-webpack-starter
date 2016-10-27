2e792597fc3ce318686b1c87998cd6ac0505fae6
paul <pvasile@pentalog.fr>
2016-10-27 17:17:03 +0300

    a

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home';
+import { GayComponent } from './gay';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
 
@@ -10,6 +11,7 @@ export const ROUTES: Routes = [
   { path: '',      component: HomeComponent },
   { path: 'home',  component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'gay', component: GayComponent },
   {
     path: 'detail', loadChildren: () => System.import('./+detail').then((comp: any) => {
       return comp.default;
